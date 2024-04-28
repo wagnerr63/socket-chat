4c84ed33b05a668e43f033d0289132e21c9e69eb
Pedro Rosário <pdro.h.rosario@gmail.com>
2024-04-28 18:08:51 -0300

    commit inicial

---
+++ src/main/java/com/chat/Client/Client.java
@@ -0,0 +1,53 @@
+package com.chat.Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        final String SERVER = "localhost";
+        final int SERVER_PORT = 12345;
+
+        try {
+            Socket socket = new Socket(SERVER, SERVER_PORT);
+            System.out.println("Conectado ao servidor");
+
+            Scanner input = new Scanner(socket.getInputStream());
+            PrintStream output = new PrintStream(socket.getOutputStream());
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+            String userName = getUserName(keyboard);
+            output.println(userName);
+
+            Thread threadReceiver = new Thread(() -> receiveMessages(input));
+            threadReceiver.start();
+
+            while (true) {
+                String message = keyboard.readLine();
+                output.println(message);
+                if (message.equals("/sair")) {
+                    break;
+                }
+            }
+
+            input.close();
+            output.close();
+            keyboard.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getUserName(BufferedReader teclado) throws IOException {
+        System.out.print("Digite seu nome de usuário: ");
+        return teclado.readLine();
+    }
+
+    private static void receiveMessages(Scanner input) {
+        while (input.hasNextLine()) {
+            System.out.println(input.nextLine());
+        }
+    }
+}
---
+++ src/main/java/com/chat/Server/Server.java
@@ -0,0 +1,85 @@
+package com.chat.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Server extends Thread {
+
+    private Socket clientSocket;
+
+    private static Map<String, Socket> clients = new HashMap<>();
+
+    public Server(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        System.out.println("Servidor iniciado.\n Aguardando conexões.\n");
+
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Novo cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
+
+                logConnection(clientSocket.getInetAddress().getHostAddress());
+
+                new Server(clientSocket).start();
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    public void run() {
+        try {
+            Scanner clientOutput = new Scanner(clientSocket.getInputStream());
+            PrintStream saidaCliente = new PrintStream(clientSocket.getOutputStream());
+
+            while (true) {
+                String clientMessage = clientOutput.nextLine();
+                if (clientMessage.startsWith("/send")) {
+                    processMessage(clientMessage);
+                } else if (clientMessage.equals("/users")) {
+                    listClients(saidaCliente);
+                } else if (clientMessage.equals("/sair")) {
+                    clientSocket.close();
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processMessage(String senderMessage) throws IOException {
+        String[] parts = senderMessage.split(" ");
+        String command = parts[0];
+        String receiver = parts[2];
+
+    }
+
+    private void listClients(PrintStream receiver) throws IOException {
+        
+    }
+
+    private void sendMessage(Socket receiver, String message) throws IOException {
+        
+    }
+
+    private void sendFile(Socket destinatario, String caminhoArquivo) throws IOException {
+        
+    }
+
+    private static void logConnection(String adrressIP) throws IOException {
+        
+    }
+}
